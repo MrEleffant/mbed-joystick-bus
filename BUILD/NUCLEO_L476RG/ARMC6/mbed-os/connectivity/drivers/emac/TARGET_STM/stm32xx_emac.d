d9ea698110b00cd16e189cf78282469c749edcef
MrEleffant <90548369+MrEleffant@users.noreply.github.com>
2024-01-26 10:40:58 +0100

    Delete BUILD directory

--- BUILD/NUCLEO_L476RG/ARMC6/mbed-os/connectivity/drivers/emac/TARGET_STM/stm32xx_emac.d
+++
@@ -1,3 +0,0 @@
-BUILD\NUCLEO_L476RG\ARMC6\mbed-os\connectivity\drivers\emac\TARGET_STM\stm32xx_emac.o: \
-  mbed-os\connectivity\drivers\emac\TARGET_STM\stm32xx_emac.cpp \
-  BUILD\NUCLEO_L476RG\ARMC6\mbed_config.h
